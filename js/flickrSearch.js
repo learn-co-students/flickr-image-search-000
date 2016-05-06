726b508c8d8e9e912bf630adf7a4b4c39c9b9336
NoneOfMaster <rcraigt@gmail.com>
2016-05-06 15:29:44 -0700

    my fancyboxes are fancy

--- js/flickrSearch.js
+++ js/flickrSearch.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
   
-  //write your solution here...
+    setInputListener();
     
 });
+
+function setInputListener(){
+    $("button").click(function(){
+        var imageTag = captureInput();
+        makeAjaxRequest(imageTag, success());
+    });
+};
+
+function success(){
+    return function(json){
+        debugger
+        var photos = json.photos.photo;
+        for (photo in photos) {
+            var url = "http://farm" + photos[photo].farm + ".staticflickr.com/" + photos[photo].server + "/" + photos[photo].id + "_" + photos[photo].secret + ".jpg";
+            $("#feed").append("<a class='fancybox' rel='group' href='" + url + "'><img src='" + url + "' height='40' width='40'></a>");
+        }
+        $(".fancybox").fancybox();
+    }
+}
+
+function captureInput(){
+    return $("input").val();
+};
+
+function makeAjaxRequest(tag, success){
+    $.ajax({
+        url: 'https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=2fd41b49fedfd589dc265350521ab539&tags='+ tag +'&jsoncallback=?',
+        dataType: "json",
+        success: success
+    });
+};
 
 /*
 
